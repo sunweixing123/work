516d2be594542c77e08ed6daed1e4b90a93e0a63
sunweixing123 <359561000@qq.com>
2018-11-28 17:22:34 +0800

    fix:推荐

--- my-app/src/containers/Home/index.jsx
+++ my-app/src/containers/Home/index.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import HomeHeader from '../../components/HomeHeader'
 import Category from '../../components/Category'
+import Ad from './subpage/Ad'
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-    }
     render() {
         return (
             <div>
                 <HomeHeader cityName={this.props.userinfo.cityName} />
                 <Category />
+                <div style={{height: '15px'}}></div>
+                <Ad />
             </div>
         )
     }
---
+++ my-app/src/containers/Home/subpage/Ad.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Ad extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div>Ad</div>
+    )
+  }
+}
+export default Ad;
