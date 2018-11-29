70357d8895abb5025f2ef235c147e15044956dd9
sunweixing123 <359561000@qq.com>
2018-11-29 20:02:44 +0800

    fix:获取当前城市

--- my-app/src/containers/City/index.jsx
+++ my-app/src/containers/City/index.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as userInfoActionsFromOtherFile from '../../actions/userinfo'
 import Header from '../../components/Header';
 import CityList from '../../components/CityList';
+import LocalStore from '../../util/localStore'
+import {CITYNAME} from '../../config/localStoreKey'; 
 
 class City extends Component {
+    constructor(props) {
+      super(props);
+      this.changeCity = this.changeCity.bind(this);
+    }
     componentDidMount() {
+      console.log(this.props);
     }
     // mapStateToProps(state) {
     //     return {
@@ -18,6 +28,21 @@ class City extends Component {
     callBack() {
       window.history.back()
     }
+    changeCity(newCity) {
+      if(newCity == null) {
+        return;
+      }
+      //修改redux
+      const userinfo = this.props.userinfo;
+      userinfo.cityName = newCity;
+      this.props.userInfoActions.update(userinfo);
+
+      //修改localstorage
+      LocalStore.setItem(CITYNAME, newCity);
+
+
+      //跳转到首页
+    }
     render() {
         return (
             <div className="cityDetail">
@@ -26,10 +51,27 @@ class City extends Component {
               <div>北京</div>
               <div>深圳</div> */}
               <Header title="选择城市" />
-              <CityList />
+              <div>{this.props.userinfo.cityName}</div>
+              <CityList changeFn={this.changeCity} />
 
             </div>
         )
     }
 }
-export default City;
+// export default City;
+
+function mapStateToProps(state) {
+    return {
+        userinfo: state.userinfo
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch)
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(City)
