7c9d8eae04d73e0d333d9ab6cd0f60ff0586aac1
sunweixing123 <359561000@qq.com>
2018-12-04 21:47:22 +0800

    fix:首页组件

---
+++ my-app/src/components/HomeAd/index.jsx
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react';
+import './index.less';
+class HomeAd extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>test</div>
+        )
+    }
+}
+
+export default HomeAd;
