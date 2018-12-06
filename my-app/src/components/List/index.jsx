edfe2f0a394ec9019a02f1e8ba374dac6f1139f0
sunweixing123 <359561000@qq.com>
2018-12-06 22:05:38 +0800

    fix:猜你喜欢组件

---
+++ my-app/src/components/List/index.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import './index.less';
+import Item from './Item';
+
+class List extends Component {
+    render() {
+        return (
+            <div className="list-container">
+                {this.props.data.map((item, index) => {
+                    return <Item key={index} data={item}/>
+                })}
+            </div>
+        )
+    }
+}
+
+export default List;
