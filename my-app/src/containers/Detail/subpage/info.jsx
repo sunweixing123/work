cb8d5b7f547b2f1b8e20a17f005354f11b5c981a
sunweixing123 <359561000@qq.com>
2018-12-13 21:22:58 +0800

    fix:test

--- my-app/src/containers/Detail/subpage/info.jsx
+++ my-app/src/containers/Detail/subpage/info.jsx
@@ -3,7 +3,7 @@ import React,{Component} from 'react';
 class Info extends Component {
     render() {
         return (
-            <div>info</div>
+            <div>infotest</div>
         )
     }
 }
