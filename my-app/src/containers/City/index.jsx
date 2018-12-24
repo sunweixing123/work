85de32572d2e67efad78fc677b2ffb5acbee303e
sunweixing123 <359561000@qq.com>
2018-12-24 22:36:24 +0800

    fix:test

--- my-app/src/containers/City/index.jsx
+++ my-app/src/containers/City/index.jsx
@@ -47,10 +47,6 @@ class City extends Component {
     render() {
         return (
             <div className="cityDetail">
-              {/* <div onClick={this.callBack}>返回</div>
-              <h1>选择城市</h1>
-              <div>北京</div>
-              <div>深圳</div> */}
               <Header title="选择城市" />
               <div>{this.props.userinfo.cityName}</div>
               <CityList changeFn={this.changeCity} />
