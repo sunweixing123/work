afc9ce14f59957223073c13da99d2ffdc6be1892
sunweixing123 <359561000@qq.com>
2019-01-15 22:24:44 +0800

    redux

--- my-app/src/containers/Study/ReduxTest/index.jsx
+++ my-app/src/containers/Study/ReduxTest/index.jsx
@@ -1,48 +1,98 @@
+// import React, { Component } from 'react';
+// import { bindActionCreators } from 'redux';
+// import { connect } from 'react-redux';
+// import { Button } from 'antd';
+// import * as Test1 from '../../../actions/test1';
+// import * as userInfoActionsFromOtherFile from '../../../actions/userinfo'
+
+// import './index.less'
+
+// class ReduxTest extends Component {
+//   componentDidMount() {
+//   //   this.props.testActions.testUpdata({
+//   //     testData: 'firstData',
+//   // })
+//   }
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+
+//     };
+//     this.changeRedux = this.changeRedux.bind(this);
+//   }
+
+//   changeRedux() {
+//     // this.props.testActions.testUpdata({
+//     //   testData: '修改一下',
+//     // });
+//   }
+
+
+//   render() {
+//     console.log(this.props);
+//     return (
+//       <div className="reduxTest" onClick={this.changeRedux}>
+//         {this.props.testData}
+//       </div>
+//     )
+//   }
+// }
+
+// //练习绑定redux
+// function mapStateToProps(state) {
+//   console.log(state);
+//   return {
+//     userinfo: state.userinfo,
+//   }
+// }
+
+// function mapDispatchToProps(dispatch) { //把action和dispatch绑定
+//   return {
+//     testActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch),
+//   }
+// }
+
+// export default connect (
+//   mapDispatchToProps,
+//   mapStateToProps,
+// )(ReduxTest);
+
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { Button } from 'antd';
-import * as Test1 from '../../../actions/';
-import './index.less'
+import * as userInfoActionsFromOtherFile from '../../../actions/userinfo'
 
-class ReduxTest extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
-  changeRedux() {
-    this.props.userInfoActions.test1('test修改成功');
-  }
-
-
-  render() {
-    console.log(this.props);
-    return (
-      <div className="reduxTest" onClick={this.changeRedux}>
-        {this.props.testData}
-      </div>
-    )
-  }
+class City extends Component {
+    constructor(props) {
+      super(props);
+    }
+    componentDidMount() {
+      console.log(this.props);
+    }
+    render() {
+        return (
+            <div className="cityDetail">
+              test
+            </div>
+        )
+    }
 }
+// export default City;
 
-//练习绑定redux
+//绑定redux
 function mapStateToProps(state) {
-  console.log(state);
-  return {
-    testData: state.test,
-  }
+    console.log(state);
+    return {
+        userinfo: state.userinfo
+    }
 }
 
-function mapDispatchToProps(dispatch) { //把action和dispatch绑定
-  return {
-    testActions: bindActionCreators(Test1, dispatch),
-  }
+function mapDispatchToProps(dispatch) {
+    return {
+        userInfoActions: bindActionCreators(userInfoActionsFromOtherFile, dispatch) //actions和dispatch结合
+    }
 }
-
-export default connect (
-  mapDispatchToProps,
-  mapStateToProps,
-)(ReduxTest);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(City)
